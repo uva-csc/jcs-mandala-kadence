197219705acfc7314b75ebd5a9c9d127a0c05135
ThanGrove <than@virginia.edu>
2022-03-29 08:11:05 -0400

    updating styles for mandala-kadence theme

--- functions.php
+++ functions.php
@@ -6,8 +6,8 @@ class MandalaKadence {
 
 	function init() {
 		// Reigster styles and scripts for Mandala Kadence theme
-		//wp_enqueue_style( 'mandala-kadence-styles', get_stylesheet_directory_uri() . '/css/mandala-kadence.css',
-		// 	false,'1.0','all');
+		wp_enqueue_style( 'mandala-kadence-styles', get_stylesheet_directory_uri() . '/css/mandala-kadence.css',
+		 	false,'1.0','all');
 		wp_enqueue_script( 'mandala-kadence-scripts', get_stylesheet_directory_uri() . '/js/mandala-kadence.js',
 			array ( 'jquery' ), 1.0, true);
 	}
