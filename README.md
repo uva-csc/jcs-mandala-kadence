48a909408cb500d46cb9e03aa66ae3e4741bb027
ThanGrove <than@virginia.edu>
2022-03-29 06:07:44 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Mandala Kadence
+A child theme of Kadence for Mandala-based sites and their subsites
+
+**Author:** Than Grove \
+**Organization:** Contemplative Sciences Center & Tibetan and Himalayan Digital Library, University of Virginia \
+**Date:** March 29, 2022 
---
+++ functions.php
@@ -0,0 +1,17 @@
+<?php
+class MandalaKadence {
+	function __construct() {
+		add_action('after_setup_theme', array($this, 'init'));
+	}
+
+	function init() {
+		// Reigster styles and scripts for Mandala Kadence theme
+		wp_enqueue_style( 'mandala-kadence-styles', get_template_directory_uri() . '/css/mandala-kadence.css',
+			false,'1.0','all');
+		wp_enqueue_script( 'mandala-kadence-scripts', get_template_directory_uri() . '/js/mandala-kadence.js',
+			array ( 'jquery' ), 1.0, true);
+	}
+
+}
+
+$mandalakadence = new MandalaKadence();
