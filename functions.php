e74cd72e1a566f33e38a88633c56b246150d6ee6
ThanGrove <than@virginia.edu>
2023-09-21 13:42:13 -0400

    adding changes made directly to production site

--- functions.php
+++ functions.php
@@ -5,3 +5,35 @@ require_once 'class-mandala-admin.php';
 
 /* Class to extend kadence to allow for subsite banners, menus, etc. */
 require_once 'class-mandala-kadence.php';
+
+// Create Shortcode to Display Special Issue Post Types
+function create_shortcode_issues_post_type(){
+    $args = array(
+                    'post_type'      => 'special-issue',
+                    'posts_per_page' => '-1',
+                    'publish_status' => 'published',
+                 );
+  
+    $query = new WP_Query($args);
+  
+    if($query->have_posts()) :
+  
+					echo('<div id="archive-container" class="content-wrap grid-cols special-issue-archive grid-sm-col-1 grid-lg-col-1 item-image-style-beside">');
+
+        while($query->have_posts()) :
+  
+            $query->the_post() ;
+                      
+        	do_action( 'kadence_loop_entry' );
+  
+        endwhile;
+	
+	echo('</div>');
+  
+        wp_reset_postdata();
+  
+    endif;    
+  
+} 
+add_shortcode( 'issues-list', 'create_shortcode_issues_post_type' ); 
+// shortcode code ends here
---
+++ template-parts/content/entry_loop_title.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Template part for displaying a post's title
+ *
+ * @package kadence
+ */
+
+namespace Kadence;
+
+$slug              = ( is_search() ? 'search' : get_post_type() );
+$title_element = kadence()->option( $slug . '_archive_element_title', array(
+	'enabled' => true,
+) );
+if ( isset( $title_element ) && is_array( $title_element ) && true === $title_element['enabled'] ) {
+	
+	if ( get_post_type( get_the_ID() ) == 'special-issue' ) {
+		the_title( '<h2 class="entry-title">', '</h2>' );
+	}
+	else if ( is_search() || is_archive() || is_home() ) {
+		the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+	} else {
+		the_title( '<h3 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h3>' );
+	}
+}
